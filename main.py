6186af246a1c14d4fd9cf19f4d8572253e71d18a
Igor Popov <ihptru@gmail.com>
2011-10-30 20:56:19 +0300

    count time required to perform a job

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 import time
+import multiprocessing
+import datetime
 
 import remove_albums
 import remove_images
@@ -9,10 +11,21 @@ class Factory:
     
     def __init__(self):
         self.start_time = time.mktime(time.strptime( time.strftime('%Y-%m-%d-%H-%M-%S'), '%Y-%m-%d-%H-%M-%S'))
-        
+
+    def current_time(self):
+        current = time.mktime(time.strptime( time.strftime('%Y-%m-%d-%H-%M-%S'), '%Y-%m-%d-%H-%M-%S'))
+        return current
+
     def run_jobs(self):
+        multiprocessing.Process(target=self.image_factory).start()
+    
+    def image_factory(self):
         remove_albums.start(self)
         remove_images.start(self)
+        current = self.current_time()
+        difference = current - self.start_time
+        result = str(datetime.timedelta(seconds = difference))
+        print "It took %s to remove images" % result
 
 if __name__ == "__main__":
     factory = Factory()
